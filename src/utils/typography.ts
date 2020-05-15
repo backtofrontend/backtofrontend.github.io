1581e76dfe01510e189cf57452b9c137e04eab84
kappafon <goran.rihter@gmail.com>
2020-05-15 11:34:49 +0200

    gatsby v, dark-mode inception

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,13 +1,40 @@
+import '../styles/global.css'
 import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-Wordpress2016.overrideThemeStyles = () => {
-    return {
-        'a.gatsby-resp-image-link': {
-            boxShadow: `none`,
-        },
-    }
-}
+Wordpress2016.overrideThemeStyles = () => ({
+    a: {
+        color: 'var(--anchorLink)',
+    },
+    // gatsby-remark-autolink-headers - don't underline when hidden
+    'a.anchor': {
+        boxShadow: 'none',
+    },
+    // gatsby-remark-autolink-headers - use theme colours for the link icon
+    'a.anchor svg[aria-hidden="true"]': {
+        stroke: 'var(--textLink)',
+    },
+    'a.gatsby-resp-image-link': {
+        boxShadow: `none`,
+    },
+    'p code': {
+        fontSize: '1rem',
+    },
+    'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code': {
+        fontSize: 'inherit',
+    },
+    'li code': {
+        fontSize: '1rem',
+    },
+    hr: {
+        background: 'var(--hr)',
+    },
+    blockquote: {
+        color: 'inherit',
+        borderLeftColor: 'inherit',
+        opacity: '0.8',
+    },
+})
 
 delete Wordpress2016.googleFonts
 
